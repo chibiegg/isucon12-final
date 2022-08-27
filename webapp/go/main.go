54b8ecd52e8cacfa5d5afa08d4bb738e8374ff5c
a01-20 <chibiegg@chibiegg.net>
2022-08-27 08:56:17 +0000

    fix

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -20,7 +20,6 @@ import (
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/labstack/gommon/log"
 	"github.com/pkg/errors"
 	"golang.org/x/sync/errgroup"
 )
@@ -126,7 +125,7 @@ func main() {
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Output: io.Discard,
 	}))
-	e.Logger.SetLevel(log.OFF)
+	e.Logger.SetLevel(0)
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
