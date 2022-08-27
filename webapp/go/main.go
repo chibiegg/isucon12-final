2193575d628f9dc5964b195f6f4e3ec53417a39f
chibiegg <chibiegg@chibiegg.net>
2022-08-27 13:13:36 +0900

    fix tx2

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -1242,7 +1242,7 @@ func (h *Handler) drawGacha(c echo.Context) error {
 	}
 	defer tx.Rollback() //nolint:errcheck
 
-	tx2, err := h.DB.Beginx()
+	tx2, err := h.DB2.Beginx()
 	if err != nil {
 		return errorResponse(c, http.StatusInternalServerError, err)
 	}
@@ -1407,7 +1407,7 @@ func (h *Handler) receivePresent(c echo.Context) error {
 	}
 	defer tx.Rollback() //nolint:errcheck
 
-	tx2, err := h.DB.Beginx()
+	tx2, err := h.DB2.Beginx()
 	if err != nil {
 		return errorResponse(c, http.StatusInternalServerError, err)
 	}
