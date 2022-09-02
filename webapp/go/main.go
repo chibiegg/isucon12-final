e9bf0257f455422bb8eb72a23166fb9cef89f952
314@live.jp <__math>
2022-09-03 00:52:34 +0900

    more max connections

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -217,12 +217,12 @@ func main() {
 	e.Logger.Error(e.StartServer(e.Server))
 }
 
-func connectDB1(batch bool) (*sqlx.DB, error) {
+func connectDB(host string, batch bool) (*sqlx.DB, error) {
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=%s&multiStatements=%t&interpolateParams=true",
 		getEnv("ISUCON_DB_USER", "isucon"),
 		getEnv("ISUCON_DB_PASSWORD", "isucon"),
-		getEnv("ISUCON_DB_HOST1", "127.0.0.1"),
+		host,
 		getEnv("ISUCON_DB_PORT", "3306"),
 		getEnv("ISUCON_DB_NAME", "isucon"),
 		"Asia%2FTokyo",
@@ -232,61 +232,24 @@ func connectDB1(batch bool) (*sqlx.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+	dbx.SetMaxIdleConns(100)
 	return dbx, nil
+}
+
+func connectDB1(batch bool) (*sqlx.DB, error) {
+	return connectDB(getEnv("ISUCON_DB_HOST1", "127.0.0.1"), batch)
 }
 
 func connectDB2(batch bool) (*sqlx.DB, error) {
-	dsn := fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=%s&multiStatements=%t&interpolateParams=true",
-		getEnv("ISUCON_DB_USER", "isucon"),
-		getEnv("ISUCON_DB_PASSWORD", "isucon"),
-		getEnv("ISUCON_DB_HOST2", "127.0.0.1"),
-		getEnv("ISUCON_DB_PORT", "3306"),
-		getEnv("ISUCON_DB_NAME", "isucon"),
-		"Asia%2FTokyo",
-		batch,
-	)
-	dbx, err := sqlx.Open("mysql", dsn)
-	if err != nil {
-		return nil, err
-	}
-	return dbx, nil
+	return connectDB(getEnv("ISUCON_DB_HOST2", "127.0.0.1"), batch)
 }
 
 func connectDB3(batch bool) (*sqlx.DB, error) {
-	dsn := fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=%s&multiStatements=%t&interpolateParams=true",
-		getEnv("ISUCON_DB_USER", "isucon"),
-		getEnv("ISUCON_DB_PASSWORD", "isucon"),
-		getEnv("ISUCON_DB_HOST3", "127.0.0.1"),
-		getEnv("ISUCON_DB_PORT", "3306"),
-		getEnv("ISUCON_DB_NAME", "isucon"),
-		"Asia%2FTokyo",
-		batch,
-	)
-	dbx, err := sqlx.Open("mysql", dsn)
-	if err != nil {
-		return nil, err
-	}
-	return dbx, nil
+	return connectDB(getEnv("ISUCON_DB_HOST3", "127.0.0.1"), batch)
 }
 
 func connectDB4(batch bool) (*sqlx.DB, error) {
-	dsn := fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=%s&multiStatements=%t&interpolateParams=true",
-		getEnv("ISUCON_DB_USER", "isucon"),
-		getEnv("ISUCON_DB_PASSWORD", "isucon"),
-		getEnv("ISUCON_DB_HOST4", "127.0.0.1"),
-		getEnv("ISUCON_DB_PORT", "3306"),
-		getEnv("ISUCON_DB_NAME", "isucon"),
-		"Asia%2FTokyo",
-		batch,
-	)
-	dbx, err := sqlx.Open("mysql", dsn)
-	if err != nil {
-		return nil, err
-	}
-	return dbx, nil
+	return connectDB(getEnv("ISUCON_DB_HOST4", "127.0.0.1"), batch)
 }
 
 func (h *Handler) getDatabaseForUserID(userId int64) (*sqlx.DB, *sqlx.DB) {
